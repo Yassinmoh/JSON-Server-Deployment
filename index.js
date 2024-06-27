d99acfcf527ecfea2de7e90b04e231061dab962d
Yassinmoh <yassin_mohamed310.com>
2024-06-27 22:40:58 +0300

    Add CORS middleware

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = 8080;
+const cors = require("cors");
+// Custom CORS middleware
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+// server.use(cors());
 
 server.use(middlewares);
 server.use(router);
